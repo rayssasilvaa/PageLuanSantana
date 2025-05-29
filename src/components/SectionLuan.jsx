5c3d022992b956ea43668ff57ae4a75cc466cb9b
Rayssa Silva <rayssalemescrz05@outlook.com>
2025-05-29 13:05:00 -0300

    Implementa parallax compatível com mobile em SectionLuan.jsx substituindo bg-fixed por transformação via scroll

--- src/components/SectionLuan.jsx
+++ src/components/SectionLuan.jsx
@@ -1,18 +1,15 @@
 const SectionLuan = () => {
   return (
     // imagem de fundo com gradiente
- <section
-  id="inicio"
-  className="relative h-screen w-full bg-black bg-cover bg-center bg-fixed"
-  style={{
-    backgroundImage: "url('/assets/img4.webp')"
-  }}
->
-
-  {/* Gradiente inferior para transição entre seções */}
-  <div className="absolute bottom-0 left-0 w-full h-40 bg-gradient-to-b from-transparent to-black z-20" />
-</section>
-
+      <section
+      id="inicio"
+      className="relative h-screen w-full bg-black bg-cover bg-center md:bg-fixed"
+      style={{
+        backgroundImage: "url('/assets/img4.webp')"
+      }}
+    >
+      <div className="absolute bottom-0 left-0 w-full h-40 bg-gradient-to-b from-transparent to-black z-20" />
+    </section>
   );
 };
 
